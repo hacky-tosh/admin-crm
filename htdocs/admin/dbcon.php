3cf2c597ba9cc9561008c8ba51ed77e1ff40c4ed
ashutosh <ashutosh.sahu@agamitechnologies.in>
2023-11-08 23:44:59 +0530

    -am.

--- htdocs/admin/dbcon.php
+++ htdocs/admin/dbcon.php
@@ -1,3 +1,3 @@
 <?php
-$conn = mysqli_connect('134.209.212.95','ashu','root','capstone') or die(mysqli_error());
+$conn = mysqli_connect('retrievefit.com','ashu','root','capstone') or die(mysqli_error());
 ?>
